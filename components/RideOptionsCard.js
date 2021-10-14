777296f4fea214d3d3cde7a19b0716c7c4a9b808
Kenny Zhao <53667317+mzha999@users.noreply.github.com>
2021-10-15 09:11:17 +1300

    uber update

---
+++ components/RideOptionsCard.js
@@ -0,0 +1,91 @@
+import { useNavigation } from "@react-navigation/core";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  SafeAreaView,
+  TouchableOpacity,
+  View,
+  Image,
+} from "react-native";
+import { Icon } from "react-native-elements";
+import { FlatList } from "react-native-gesture-handler";
+import { useSelector } from "react-redux";
+import tw from "tailwind-react-native-classnames";
+import { selectTravelTimeInfo, setTravelTimeInfo } from "../slices/navSlice";
+
+const data = [
+  {
+    id: "Uber-X-123",
+    title: "UberX",
+    multiplier: 1,
+    image: "https://links.papareact.com/3pn",
+  },
+  {
+    id: "Uber-XL-456",
+    title: "Uber XL",
+    multiplier: 1.2,
+    image: "https://links.papareact.com/5w8",
+  },
+  {
+    id: "Uber-LUX-789",
+    title: "Uber LUX",
+    multiplier: 1.75,
+    image: "https://links.papareact.com/7pf",
+  },
+];
+const SURGE_CHANGE_RATE = 1.5;
+const RideOptionsCard = () => {
+  const navigation = useNavigation();
+  const [selected, setSelected] = useState(null)
+  const travelTimeInfo = useSelector(selectTravelTimeInfo)
+  return (
+    <SafeAreaView style={tw`bg-white flex-grow`}>
+      <View>
+        <TouchableOpacity
+          style={tw`absolute top-3 left-5 z-50 p-3 rounded-full`}
+          onPress={() => navigation.navigate("NavigateCard")}
+        >
+          <Icon name="chevron-left" type="fontawesome" />
+        </TouchableOpacity>
+        <Text style={tw`text-center py-5 text-xl`}> Select a Ride - {travelTimeInfo?.distance?.text}</Text>
+      </View>
+      <FlatList
+        data={data}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <TouchableOpacity
+          onPress={()=>{setSelected(item)}}
+            style={tw`flex-row justify-between items-center px-10 ${item.id===selected?.id && "bg-gray-200"}`}
+          >
+            <Image
+              style={{ width: 100, height: 100, resizeMode: "contain" }}
+              source={{ uri: item.image }}
+            />
+            <View style={tw`-ml-6`}>
+              <Text style={tw`text-xl font-semibold`}>{item.title}</Text>
+              <Text>{travelTimeInfo?.duration?.text} Travel Time</Text>
+            </View>
+            <Text style={tw`text-xl`}>
+                {new Intl.NumberFormat('en-gb', {
+                    style: 'currency',
+                    currency: 'USD'
+                }).format((travelTimeInfo.duration?.value * SURGE_CHANGE_RATE * item.multiplier)/100)}
+            </Text>
+          </TouchableOpacity>
+        )}
+      />
+      <View style={tw`mt-auto border-t border-gray-200`}>
+          <TouchableOpacity disabled={!selected} style={tw`bg-black py-3 m-3 ${!selected && "bg-gray-300"}`}>
+              <Text style={tw`text-center text-white text-xl`}>
+                  Choose {selected?.title}
+              </Text>
+          </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default RideOptionsCard;
+
+const styles = StyleSheet.create({});
---
+++ screens/EatsScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, SafeAreaView, Text } from "react-native";
+import tw from "tailwind-react-native-classnames";
+const EatsScreen = () => {
+    return (
+        <SafeAreaView>
+            <Text>Hello eat</Text>
+        </SafeAreaView>
+    )
+}
+
+export default EatsScreen
+
+const styles= StyleSheet.create({})
